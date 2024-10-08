89adcfa1258b89458917bf357159779b8a1c7b97
EdyFS <edy.ferreira.santana@gmail.com>
2024-10-07 21:22:39 -0300

    Edit

--- resources/views/cliente/edit.blade.php
+++ resources/views/cliente/edit.blade.php
@@ -2,13 +2,13 @@
 
     <h5>Alterar Categoria</h5>
 
-    <form action="/categoria/" method="POST">
+    <form action="/cliente/{{$cliente->id}}" method="POST">
         @CSRF
         @method('PUT')
         <div class="row">
             <div class="col">
                 <label for="nome" class="form-label">Informe a categoria:</label>
-                <input type="text" name="nome" class="form-control"/>
+                <input type="text" name="nome" class="form-control" value="{{$cliente->nome}}"/>
             </div>
         </div>
         <div class="row">
