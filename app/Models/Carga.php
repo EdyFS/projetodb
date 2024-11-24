538ae71c7a42d918c6eed84012281b40e0b415fe
EdyFS <edy.ferreira.santana@gmail.com>
2024-11-24 16:32:10 -0300

    Alterações em carga

--- app/Models/Carga.php
+++ app/Models/Carga.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Cliente;
+use App\Models\Motorista;
 
 class Carga extends Model
 {
-    protected $fillable = ['nomeCarga','peso','tamanho','fragilidade']; //só o que for manipulável
+    protected $fillable = ['nomeCarga','peso','tamanho','fragilidade', 'cliente_id', 'motorista_id']; //só o que for manipulável
 
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function motorista()
+    {
+        return $this->belongsTo(Motorista::class);
+    }
 }
--- database/migrations/2024_11_24_183910_add_clienteid_and_motoristaid_to_cargas_table.php
+++ database/migrations/2024_11_24_183910_add_clienteid_and_motoristaid_to_cargas_table.php
@@ -12,8 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('cargas', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('cliente_id');
             $table->foreign('cliente_id')
                     ->references('id')
                     ->on("clientes")
